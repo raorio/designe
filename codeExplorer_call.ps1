eb21df4af51231340a7c85a979f5a70f257fb577
raorio <reorio.hk@gmail.com>
2019-07-11 04:35:45 +0900

    add near match function
    add near match function

--- codeExplorer_call.ps1
+++ codeExplorer_call.ps1
@@ -1,3 +1,12 @@
+Param(
+  [string]$mode = "import",
+  [string]$outDir = $PSScriptRoot
+  #[string]$outDir = Split-Path $MyInvocation.MyCommand.Path
+  #[string]$outDir = $MyInvocation.MyCommand.Name
+)
+Write-Host $mode
+Write-Host $outDir
+
 filter ImportCSV($Path) {
   $DataArray = Import-Csv $Path
   $DataArray
@@ -21,6 +30,7 @@ filter GetApplicationWindow($ProcessNames) {
     }
   }
 }
+
 filter TestGetApplicationWindow() {
   $ProcessNames = @("*")
   date
@@ -129,19 +139,31 @@ filter GetExplorerWindowRect($ProcessNames, [String]$WindowName, [String]$PathNa
 }
 
 # ExplorerとApplicationが類似(X,Y、Left,Topが異なったり、Rect変更が異なる)の処理なので、まとめる
-filter SetExplorerWindowRect($SetWindowDataArray, $CurrentWindowArray) {
+filter SetExplorerWindowRect($SetWindowDataArray, $CurrentWindowArray, $IsDoneFlagCurrent, $IsDoneFlagData) {
   #Write-Host "SetExplorerWindowRect() start"
+  
+  # 完全一致するものを実施
   foreach ($CurrentWindow in $CurrentWindowArray) {
-    $isDetectChangeEqual = $False
+    #Write-Host "target app name"; Write-Host $CurrentWindow.Name
     $isActiveCurrent = IsActiveWindow ($CurrentWindow)
     if ($isActiveCurrent -eq $False) {
       #Write-Host "skip not active windows"; Write-Host $CurrentWindow.Left; Write-Host ", "; Write-Host $CurrentWindow.Top; $CurrentWindow
       continue
     }
+    if ($IsDoneFlagCurrent[$CurrentWindow.HWND] -eq $True) {
+      #Write-Host "skip already set data"; Write-Host $CurrentWindow.HWND
+      continue
+    }
+    $isDetectChangeEqual = $False
+    $isActiveCurrent = IsActiveWindow ($CurrentWindow)
     foreach ($SetWindowData in $SetWindowDataArray) {
       $isActiveData = IsActiveData ($SetWindowData)
       if ($isActiveData -eq $False) {
         #Write-Host "skip not active data"; Write-Host $SetWindowData.X; Write-Host ", "; Write-Host $SetWindowData.Y; $SetWindowData
+        continue
+      }
+      if ($IsDoneFlagData[$SetWindowData.HWND] -eq $True) {
+        #Write-Host "skip already set data"; Write-Host $SetWindowData.HWND
         continue
       }
       $isEqual = IsEqualExplorerWindow ($SetWindowData) ($CurrentWindow)
@@ -149,13 +171,19 @@ filter SetExplorerWindowRect($SetWindowDataArray, $CurrentWindowArray) {
         $isDetectChangeEqual = $True
         #Write-Host "detect equal"; Write-Host $SetWindowData.locationName; Write-Host $CurrentWindow.locationName
         ChangeExplorerWindow ($SetWindowData) ($CurrentWindow)
+        # フラグをセット
+        $IsDoneFlagCurrent.remove($CurrentWindow.HWND)
+        $IsDoneFlagCurrent.add($CurrentWindow.HWND, $True)
+        $IsDoneFlagData.remove($SetWindowData.HWND)
+        $IsDoneFlagData.add($SetWindowData.HWND, $True)
+        break
       } else {
         #Write-Host "don't detect equal"; Write-Host $SetWindowData.locationName; Write-Host $CurrentWindow.locationName
       }
     }
     if ($isDetectChangeEqual -eq $False) {
       # 一致するウィンドウが見つけられなかった
-      # あいまい検索を実行
+      #Write-Host "not detect equal"
     }
   }
 }
@@ -174,16 +202,22 @@ filter IsActiveWindow($CurrentWindow) {
   return
 }
 
-filter SetApplicationWindowRect($SetWindowDataArray, $CurrentWindowArray) {
+filter SetApplicationWindowRect($SetWindowDataArray, $CurrentWindowArray, $IsDoneFlagCurrent, $IsDoneFlagData) {
   #Write-Host "SetExplorerApplicationRect() start"
+  
+  # 完全一致するものを実施
   foreach ($CurrentWindow in $CurrentWindowArray) {
     #Write-Host "target app name"; Write-Host $CurrentWindow.Name
-    $isDetectChangeEqual = $False
     $isActiveCurrent = IsActiveApplication ($CurrentWindow)
     if ($isActiveCurrent -eq $False) {
       #Write-Host "skip not active windows"; Write-Host $CurrentWindow.Left; Write-Host ", "; Write-Host $CurrentWindow.Top; $CurrentWindow
       continue
     }
+    if ($IsDoneFlagCurrent[$CurrentWindow.HWND] -eq $True) {
+      #Write-Host "skip already set data"; Write-Host $CurrentWindow.HWND
+      continue
+    }
+    $isDetectChangeEqual = $False
     foreach ($SetWindowData in $SetWindowDataArray) {
       #Write-Host "target data name"; Write-Host $SetWindowData.Name
       $isActiveData = IsActiveData ($SetWindowData)
@@ -191,18 +225,76 @@ filter SetApplicationWindowRect($SetWindowDataArray, $CurrentWindowArray) {
         #Write-Host "skip not active data"; Write-Host $SetWindowData.X; Write-Host ", "; Write-Host $SetWindowData.Y; $SetWindowData
         continue
       }
+      if ($IsDoneFlagData[$SetWindowData.HWND] -eq $True) {
+        #Write-Host "skip already set data"; Write-Host $SetWindowData.HWND
+        continue
+      }
       $isEqual = IsEqualExplorerWindow ($SetWindowData) ($CurrentWindow)
       if ($isEqual -eq $True) {
         $isDetectChangeEqual = $True
-        #Write-Host "detect equal"; Write-Host $SetWindowData.locationName; Write-Host $CurrentWindow.locationName
+        #Write-Host "detect equal"; Write-Host $SetWindowData.locationName; Write-Host $CurrentWindow.locationName; Write-Host $CurrentWindow.HWND; Write-Host $SetWindowData.HWND
         ChangeApplicationWindow ($SetWindowData) ($CurrentWindow)
+        # フラグをセット
+        $IsDoneFlagCurrent.remove($CurrentWindow.HWND)
+        $IsDoneFlagCurrent.add($CurrentWindow.HWND, $True)
+        $IsDoneFlagData.remove($SetWindowData.HWND)
+        $IsDoneFlagData.add($SetWindowData.HWND, $True)
+        break
       } else {
         #Write-Host "don't detect equal"; Write-Host $SetWindowData.locationName; Write-Host $CurrentWindow.locationName
       }
     }
     if ($isDetectChangeEqual -eq $False) {
       # 一致するウィンドウが見つけられなかった
-      # あいまい検索を実行
+      #Write-Host "not detect equal"
+    }
+  }
+}
+
+filter SetNearApplicationWindowRect($SetWindowDataArray, $CurrentWindowArray, $IsDoneFlagCurrent, $IsDoneFlagData) {
+  #Write-Host "SetNearApplicationWindowRect() start"
+  
+  # 部分一致するものを実施
+  foreach ($CurrentWindow in $CurrentWindowArray) {
+    #Write-Host "target app name"; Write-Host $CurrentWindow.Name
+    $isActiveCurrent = IsActiveApplication ($CurrentWindow)
+    if ($isActiveCurrent -eq $False) {
+      #Write-Host "skip not active windows"; Write-Host $CurrentWindow.Left; Write-Host ", "; Write-Host $CurrentWindow.Top; $CurrentWindow
+      continue
+    }
+    if ($IsDoneFlagCurrent[$CurrentWindow.HWND] -eq $True) {
+      #Write-Host "skip already set data"; Write-Host $CurrentWindow.HWND
+      continue
+    }
+    $isDetectChangeEqual = $False
+    foreach ($SetWindowData in $SetWindowDataArray) {
+      #Write-Host "target data name"; Write-Host $SetWindowData.Name
+      $isActiveData = IsActiveData ($SetWindowData)
+      if ($isActiveData -eq $False) {
+        #Write-Host "skip not active data"; Write-Host $SetWindowData.X; Write-Host ", "; Write-Host $SetWindowData.Y; $SetWindowData
+        continue
+      }
+      if ($IsDoneFlagData[$SetWindowData.HWND] -eq $True) {
+        #Write-Host "skip already set data"; Write-Host $SetWindowData.HWND
+        continue
+      }
+      $isEqual = IsNearEqualExplorerWindow ($SetWindowData) ($CurrentWindow)
+      if ($isEqual -eq $True) {
+        $isDetectChangeEqual = $True
+        #Write-Host "detect equal"; Write-Host $SetWindowData.locationName; Write-Host $CurrentWindow.locationName; Write-Host $CurrentWindow.HWND; Write-Host $SetWindowData.HWND
+        ChangeApplicationWindow ($SetWindowData) ($CurrentWindow)
+        # フラグをセット
+        $IsDoneFlagCurrent.remove($CurrentWindow.HWND)
+        $IsDoneFlagCurrent.add($CurrentWindow.HWND, $True)
+        $IsDoneFlagData.remove($SetWindowData.HWND)
+        $IsDoneFlagData.add($SetWindowData.HWND, $True)
+        break
+      } else {
+        #Write-Host "don't detect equal"; Write-Host $SetWindowData.locationName; Write-Host $CurrentWindow.locationName
+      }
+    }
+    if ($isDetectChangeEqual -eq $False) {
+      # 一致するウィンドウが見つけられなかった
       #Write-Host "not detect equal"
     }
   }
@@ -236,6 +328,7 @@ filter IsActiveData($SetWindowData) {
   return
 }
 
+# Explorerだけでなく、Applicationも本APIを使用
 filter IsEqualExplorerWindow($SetWindowData, $CurrentWindow) {
   #Write-Host "IsEqualExplorerWindow() start"; Write-Host "SetWindowData"; Write-Host $SetWindowData; Write-Host "CurrentWindow"; Write-Host $CurrentWindow
   $isEqual = $False
@@ -264,12 +357,59 @@ filter IsEqualExplorerWindow($SetWindowData, $CurrentWindow) {
   return $isEqual
 }
 
+filter IsNearEqualExplorerWindow($SetWindowData, $CurrentWindow) {
+  Write-Host "IsNearEqualExplorerWindow() start"; Write-Host "SetWindowData"; Write-Host $SetWindowData; Write-Host "CurrentWindow"; Write-Host $CurrentWindow
+  $isEqual = $False
+  $isEqualName = $False
+  #$isEqualLocationName = $False
+  #$isEqualLocationURL = $False
+  # アプリケーション名が一致すれば、一致と判断する
+  $currentWindowAppName = GetApplicationName $CurrentWindow.Name
+  $setWindowDataAppName = GetApplicationName $SetWindowData.Name
+  #Write-Host "currentWindowAppName: "; Write-Host $currentWindowAppName; Write-Host "setWindowDataAppName: "; Write-Host $setWindowDataAppName
+  if ($currentWindowAppName -eq $setWindowDataAppName) {
+    #Write-Host "equal application name, therefore near equal."
+    $isEqualName = $True
+  }
+  #if ($CurrentWindow.locationName -eq $SetWindowData.locationName) {
+  #  $isEqualLocationName = $True
+  #}
+  #if ($CurrentWindow.locationURL -eq $SetWindowData.locationURL) {
+  #  $isEqualLocationURL = $True
+  #}
+  #if ($isEqualName -eq $True) {
+  #  if ($isEqualLocationName -eq $True) {
+  #    if ($isEqualLocationURL -eq $True) {
+  #      #Write-Host "equal window"; Write-Host $isEqualName; Write-Host $isEqualLocationName; Write-Host $isEqualLocationURL
+  #      $isEqual = $True
+  #      return $isEqual
+  #    }
+  #  }
+  #}
+  if ($isEqualName -eq $True) {
+    $isEqual = $True
+    return $isEqual
+  }
+  #Write-Host "not equal window"
+  return $isEqual
+}
+
+filter GetApplicationName($name) {
+  # Nameを"-"で分割し、後半をアプリケーション名とする
+  $nameArray = $name -split " - "
+  #$nameArray = $name.Split(" - ") # NGうまく分割できていない
+  #Write-Host $nameArray[0]
+  #Write-Host $nameArray[$nameArray.Length - 1]
+  return $nameArray[$nameArray.Length - 1]
+}
+
 filter ChangeExplorerWindow($SetWindowData, $CurrentWindow) {
   #Write-Host "ChangeExplorerWindow() start"; Write-Host "SetWindowData"; Write-Host $SetWindowData; Write-Host "CurrentWindow"; Write-Host $CurrentWindow
   if ($CurrentWindow.Name -eq "Internet Explorer") {
     #Write-Host "change ie size"
     # サイズが3/2倍になり狂うので、2/3倍する。
     [Win32]::MoveWindow($CurrentWindow.HWND, $SetWindowData.X / 3 * 2, $SetWindowData.Y / 3 * 2, $SetWindowData.Width / 3 * 2, $SetWindowData.Height / 3 * 2, $true)
+    #[Win32]::MoveWindow($CurrentWindow.HWND, $SetWindowData.X, $SetWindowData.Y, $SetWindowData.Width, $SetWindowData.Height, $true)
   #} elseif ($CurrentWindow.Name -eq "エクスプローラー") {
   } else {
     #Write-Host "change explorer size"
@@ -344,29 +484,26 @@ Add-Type @"
 # test #
 ########
 
-Param(
-  [string]$Arg1 = $PSScriptRoot
-)
-$CurrentDirectory = $PSScriptRoot
-#Write-Host $Arg1
-#Split-Path $MyInvocation.MyCommand.Path
-#$MyInvocation.MyCommand.Name
-$FilePath = "$CurrentDirectory" + ".\explorer_windows.csv"
+$FilePath = "$outDir" + ".\explorer_windows.csv"
 
-$Export = $True
-$Inport = $False
-#$Export = $False
-#$Inport = $True
+if ($mode -eq "export") {
+  $Export = $True
+  $Inport = $False
+} else {
+  $Export = $False
+  $Inport = $True
+}
+
+# IEは、タブが複数ある場合、サイズ変更できない。MoveWindowで実施する。
+$ProcessNames = @("エクスプローラー", "Internet Explorer")
+#$ProcessNames = @("エクスプローラー")
+Write-Host "ProcessNames: "; Write-Host $ProcessNames
+foreach($ProcessName in $ProcessNames) {
+  Write-Host "ProcessName: "; Write-Host $ProcessName
+}
 
 if ($Export -eq $True) {
   $rect = $null
-  # IEは、タブが複数ある場合、サイズ変更できない。MoveWindowで実施する。
-  $ProcessNames = @("エクスプローラー", "Internet Explorer")
-  #$ProcessNames = @("エクスプローラー")
-  Write-Host "ProcessNames: "; Write-Host $ProcessNames
-  foreach($ProcessName in $ProcessNames) {
-    Write-Host "ProcessName: "; Write-Host $ProcessName
-  }
   $WindowName = ""
   #$WindowName = "*"
   #$WindowName = "PS"
@@ -374,14 +511,14 @@ if ($Export -eq $True) {
   #Write-Host "WindowName: "; Write-Host $WindowName
   $PathName = ""
   #Write-Host "PathName: "; Write-Host $PathName
-
+  
   GetExplorerWindowRect ($ProcessNames) ($WindowName) ($PathName) | Set-Variable GetWindowsDataArray
   #Write-Host "GetWindowsDataArray: "; Write-Host $GetWindowsDataArray; $GetWindowsDataArray
-
+  
   $appProcessName = @("*")
   GetApplicationWindow ($appProcessName) | Set-Variable GetApplicationDataArray
   #Write-Host "GetApplicationDataArray: "; Write-Host $GetApplicationDataArray; $GetApplicationDataArray
-
+  
   if ($GetWindowsDataArray.GetType().Name -ne "Object[]") {
     if ($GetApplicationDataArray.GetType().Name -ne "Object[]") {
       $GetWindowsApplicationDataArray = @($GetWindowsDataArray, $GetApplicationDataArray)
@@ -396,20 +533,56 @@ if ($Export -eq $True) {
     }
   }
   #Write-Host "GetWindowsApplicationDataArray: "; Write-Host $GetWindowsApplicationDataArray; $GetWindowsApplicationDataArray
-
+  
   ExportCSV ($GetWindowsApplicationDataArray) $FilePath
 }
 
 if ($Inport -eq $True) {
   ImportCSV ($FilePath) | Set-Variable LoadWindowsApplicationDataArray
   #Write-Host "LoadWindowsApplicationDataArray: "; Write-Host $LoadWindowsApplicationDataArray; $LoadWindowsApplicationDataArray
-
+  
+  $IsDoneFlagData = @{}
+  foreach ($SetWindowData in $LoadWindowsApplicationDataArray) {
+    $IsDoneFlagData.add($LoadWindowsApplicationDataArray.HWND, $False)
+  }
+  
   $CurrentWindows = GetExplorerWindowEachFilter ($ProcessNames)
-  #Write-Host "CurrentWindows"; Write-Host $CurrentWindows; $CurrentWindows
-  SetExplorerWindowRect ($LoadWindowsApplicationDataArray) ($CurrentWindows)
-
+  Write-Host "CurrentWindows"; Write-Host $CurrentWindows; $CurrentWindows
+  
+  $IsDoneFlagCurrent = @{}
+  foreach ($CurrentWindow in $CurrentWindows) {
+    if ($CurrentWindow.HWND -eq $Null) {
+      #Write-Host "$CurrentWindow.HWND is Null, skip"
+      continue
+    }
+    #Write-Host "target HWND"; Write-Host $CurrentWindow.HWND
+    if ($IsDoneFlagCurrent.containsKey($CurrentWindow.HWND) -eq $True) {
+      #Write-Host "duplicate HWND"; Write-Host $CurrentWindow.HWND
+      $IsDoneFlagCurrent.remove($CurrentWindow.HWND)
+    }
+    $IsDoneFlagCurrent.add($CurrentWindow.HWND, $False)
+  }
+  
+  SetExplorerWindowRect ($LoadWindowsApplicationDataArray) ($CurrentWindows) ($IsDoneFlagCurrent) ($IsDoneFlagData)
+  
   $appProcessName = @("*")
   GetApplicationWindow ($appProcessName) | Set-Variable GetApplicationDataArray
   #Write-Host "GetApplicationDataArray: "; Write-Host $GetApplicationDataArray; $GetApplicationDataArray
-  SetApplicationWindowRect ($LoadWindowsApplicationDataArray) ($GetApplicationDataArray)
+  
+  $IsDoneFlagCurrent = @{}
+  foreach ($CurrentApplication in $GetApplicationDataArray) {
+    if ($CurrentApplication.HWND -eq $Null) {
+      #Write-Host "$CurrentApplication.HWND is Null, skip"
+      continue
+    }
+    #Write-Host "target HWND"; Write-Host $CurrentApplication.HWND
+    if ($IsDoneFlagCurrent.containsKey($CurrentApplication.HWND) -eq $True) {
+      #Write-Host "duplicate HWND"; Write-Host $CurrentApplication.HWND
+      $IsDoneFlagCurrent.remove($CurrentApplication.HWND)
+    }
+    $IsDoneFlagCurrent.add($CurrentApplication.HWND, $False)
+  }
+  
+  SetApplicationWindowRect ($LoadWindowsApplicationDataArray) ($GetApplicationDataArray) ($IsDoneFlagCurrent) ($IsDoneFlagData)
+  SetNearApplicationWindowRect ($LoadWindowsApplicationDataArray) ($GetApplicationDataArray) ($IsDoneFlagCurrent) ($IsDoneFlagData)
 }
